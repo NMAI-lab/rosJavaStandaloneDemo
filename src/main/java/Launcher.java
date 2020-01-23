4394bac811f1bd81c108fb3d068517573622dbd0
Patrick Gavigan <pgavigan@gmail.com>
2020-01-23 14:57:56 -0500

    Added sample node
    Fixed gradle config. Don't use the node yet.

--- src/main/java/Launcher.java
+++
@@ -1,9 +0,0 @@
-
-import org.ros.RosRun;
-
-public class Launcher extends RosRun {
-
-    public static void main(String[] argv) throws Exception {
-        System.out.println("Hello world!");
-    }
-}
