4894bdbc871f9e323d887995cc09ca73f0029aa1
Patrick Gavigan <pgavigan@gmail.com>
2020-01-23 14:08:17 -0500

    Started with Hello World
    Confirm that config is good.

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,9 @@
+
+import org.ros.RosRun;
+
+public class Launcher extends RosRun {
+
+    public static void main(String[] argv) throws Exception {
+        System.out.println("Hello world!");
+    }
+}
